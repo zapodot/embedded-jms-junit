32d39e655e60fd1e1793d80a59820ade60838f85
Sondre Eikanger Kvalø <zapodot@gmail.com>
2017-08-29 10:34:58 +0200

    Improve documentation somewhat

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ This library will in the future be distributed through the [Sonatype OSS repo](h
 * version 0.1: initial release
 
 ## Usage
+**Note: The project has not been release to Maven Central yet**
 ### Add dependency
 ```xml
 <dependency>
--- src/main/java/org/zapodot/junit/jms/EmbeddedJmsRule.java
+++ src/main/java/org/zapodot/junit/jms/EmbeddedJmsRule.java
@@ -6,27 +6,50 @@ import org.junit.rules.TestRule;
 import javax.jms.ConnectionFactory;
 import java.net.URI;
 
+/**
+ * The general contract for the EmbeddedJmsRule.
+ *
+ * Example of use:
+ * <code>
+ *      public class MyTests {
+ *
+ *          &#x00040;Rule
+ *          public EmbeddedJmsRule embeddedJmsRule = EmbeddedJmsRule.builder().build();
+ *
+ *          &#x00040;Test
+ *          public void testJmsIntegration() {
+ *              final ConnectionFactory connectionFactory = embeddedJmsRule.connectionFactory();
+ *              // Do stuff
+ *              // ...
+ *          }
+ *
+ *      }
+ * </code>
+ */
 public interface EmbeddedJmsRule extends TestRule {
+
+    static EmbeddedJmsRuleBuilder builder() {
+        return new EmbeddedJmsRuleBuilder();
+    }
 
     /**
      * Creates a ConnectionFactory that connects to the embedded broker
+     *
      * @return a fresh instance of {@link ConnectionFactory}
      */
     ConnectionFactory connectionFactory();
 
     /**
      * Creates a {@link ActiveMQConnectionFactory} for those who needs access to the AMQ specific interface
+     *
      * @return a fresh instance of {@link ActiveMQConnectionFactory}
      */
     ActiveMQConnectionFactory activeMqConnectionFactory();
 
     /**
      * An URI that may be used to connect to the embedded broker
+     *
      * @return a {@link URI}
      */
     URI brokerUri();
-
-    static EmbeddedJmsRuleBuilder builder() {
-        return new EmbeddedJmsRuleBuilder();
-    }
 }
