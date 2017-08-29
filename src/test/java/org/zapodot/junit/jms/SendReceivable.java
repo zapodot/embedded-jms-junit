f31f025428d7c5d0fb283aabc6e2f1bd75714653
Sondre Eikanger Kvalø <zapodot@gmail.com>
2017-08-29 09:16:45 +0200

    reformat

--- src/test/java/org/zapodot/junit/jms/SendReceivable.java
+++ src/test/java/org/zapodot/junit/jms/SendReceivable.java
@@ -8,13 +8,16 @@ import static org.zapodot.junit.jms.CallablesForSendingAndReceiving.messagesFrom
 import static org.zapodot.junit.jms.CallablesForSendingAndReceiving.sendMessageToQueue;
 
 public interface SendReceivable {
-    default String sendReceive(final ConnectionFactory connectionFactory, final String queueName, final String textMessage) throws JMSException {
+    default String sendReceive(final ConnectionFactory connectionFactory,
+                               final String queueName,
+                               final String textMessage) throws JMSException {
         final ExecutorService executorService = Executors.newFixedThreadPool(2);
         try {
-        final Future<String> messageFuture = executorService.submit(messagesFromTestQueue(connectionFactory, queueName));
+            final Future<String> messageFuture = executorService.submit(messagesFromTestQueue(connectionFactory,
+                                                                                              queueName));
             executorService.submit(sendMessageToQueue(connectionFactory, queueName, textMessage));
             return messageFuture.get(30L, TimeUnit.SECONDS);
-        } catch (InterruptedException|ExecutionException|TimeoutException e) {
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new IllegalStateException("An error occurred during send/receive from JMS", e);
         } finally {
             executorService.shutdownNow();
